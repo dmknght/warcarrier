2ea7c28c92672537f7e7df58b13bf93b91c58360
dmknght <dmknghtx2team@gmail.com>
2017-10-18 22:52:58 +0700

    wget wrong option

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo cp -r lib /usr/share/perl/5.26
 
-wget http://search.cpan.org/CPAN/authors/id/I/IG/IGUTHRIE/Net-Bluetooth-0.40.tar.gz -o /tmp/temp_net.tar.gz
+wget http://search.cpan.org/CPAN/authors/id/I/IG/IGUTHRIE/Net-Bluetooth-0.40.tar.gz -O /tmp/temp_net.tar.gz
 cd /tmp
 tar -xvzf temp_net.tar.gz
 cd Net-Bluetooth-0.40
